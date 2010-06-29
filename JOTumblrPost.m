3e7939cf725d079e1e690beac641b790f8fb0430
Jeremy <jeremyfoo@gmail.com>
2010-06-30 00:52:31 +0800

    C preprocessor for "overloaded properties"

--- JOTumblrPost.m
+++ JOTumblrPost.m
@@ -9,5 +9,29 @@
 #import "JOTumblrPost.h"
 
 @implementation JOTumblrPost
+@synthesize title, body, data, url;
+
+// overloaded title properties
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(source,setSource,title,NSString)
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(name,setName,title,NSString)
+							
+// overloaded body properties
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(caption,setCaption,body,NSString)
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(quote,setQuote,body,NSString)
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(description,setdescription,body,NSString)
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(conversation,setconversation,body,NSString)
+
+// overloaded url properties
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(clickThroughUrl,setClickThroughUrl,url,NSString)
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(externallyHostedUrl,setExternallyHostedUrl,url,NSString)
+SYNTHESIZE_GETTERSETTER_FOR_PROPERTY(embed,setEmbed,url,NSString)
+
+-(void)dealloc {
+	[title release], title = nil;
+	[body release], body = nil;
+	[data release], data = nil;
+	[url release], url = nil;
+	[super dealloc];
+}
 
 @end
