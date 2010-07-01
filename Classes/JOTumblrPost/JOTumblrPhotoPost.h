0c67290b733214f19da8c9f308aad09d67a97ece
Jeremy <jeremyfoo@gmail.com>
2010-07-01 15:30:05 +0800

    added MIT license, properties and NSXMLParserDelegate protocol.

--- Classes/JOTumblrPost/JOTumblrPhotoPost.h
+++ Classes/JOTumblrPost/JOTumblrPhotoPost.h
@@ -2,21 +2,45 @@
 //  JOTumblrPhotoPost.h
 //  JOTumblr
 //
-//  Created by Jeremy Foo on 6/30/10.
-//  Copyright 2010 ORNYX. All rights reserved.
+//  Created by Jeremy Foo on 3/24/10.
+//  Copyright 2010 THIRDLY. All rights reserved.
 //
+//  The MIT License
+//  
+//  Copyright (c) 2010 Jeremy Foo
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 #import "JOTumblrPost.h"
 
-@interface JOTumblrPhotoPost : JOTumblrPost {
+@interface JOTumblrPhotoPost : JOTumblrPost <NSXMLParserDelegate> {
 	NSString *source;
 	NSString *caption;
-	NSString *clickThroughURL;
+	NSURL *clickThroughURL;
 	NSData *data;
+	
+	NSMutableArray *photoURL;
 }
 @property (nonatomic, retain) NSString *source;
 @property (nonatomic, retain) NSString *caption;
 @property (nonatomic, retain) NSData *data;
-@property (nonatomic, retain) NSString *clickThroughURL;
+@property (nonatomic, retain) NSURL *clickThroughURL;
+@property (readonly) NSMutableArray *photoURL;
 @end
