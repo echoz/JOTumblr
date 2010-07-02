7fab7e73c9cb50345bb4c75082becdd7ecd8a674
Jeremy <jeremyfoo@gmail.com>
2010-07-02 13:01:00 +0800

    added more properties as well as a method to "copy" base class details to be used by a subclass.

--- Classes/JOTumblrPost.h
+++ Classes/JOTumblrPost.h
@@ -2,14 +2,40 @@
 //  JOTumblrPost.h
 //  JOTumblr
 //
-//  Created by Jeremy Foo on 5/12/10.
-//  Copyright 2010 ORNYX. All rights reserved.
+//  Created by Jeremy Foo on 3/24/10.
+//  Copyright 2010 THIRDLY. All rights reserved.
 //
+//  The MIT License
+//  
+//  Copyright (c) 2010 Jeremy Foo
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
 
-@interface JOTumblrPost : NSObject {
+@interface JOTumblrPost : NSObject <NSXMLParserDelegate> {
+	
+	// post specific
+	
+	NSString *postid;
+	NSString *apiendpoint;
 	
 	// global api
 	
@@ -26,19 +52,36 @@
 	
 	NSString *state;
 	BOOL sendToTwitter;
+
+	//xml dashboard read
+	BOOL liked;
+	NSUInteger noteCount;
+	NSString *tumblelog;
+	NSURL *rebloggedFromURL;		// where the content was from
+	NSString *rebloggedFromName;
+	NSString *rebloggedFromTitle;
+	NSURL *rebloggedRootURL;		// where the user's primary blog is
+	NSString *rebloggedRootName;
+	NSString *rebloggedRootTitle;
 	
 	// xml specific extras
-	
 	BOOL bookmarklet;
 	NSDate *dateGMT;
 	NSUInteger unixtimestamp;
 	NSString *reblogKey;
-	NSString *posturl;
-	NSString *urlWithSlug;
+	NSURL *posturl;
+	NSURL *urlWithSlug;
 	NSUInteger width;
 	NSUInteger height;
+	
+	@protected
+	NSString *currentElement;
+	NSMutableString *currentString;
 }
 
+@property (readonly) NSString *apiendpoint;
+
+@property (nonatomic, retain) NSString *postid;
 @property (nonatomic, retain) NSString *type;
 @property (nonatomic, retain) NSString *generator;
 @property (nonatomic, retain) NSDate *date;
@@ -51,13 +94,26 @@
 @property (nonatomic, retain) NSString *state;
 @property (readwrite) BOOL sendToTwitter;
 
+@property (readonly) BOOL liked;
+@property (readonly) NSUInteger noteCount;
+@property (readonly) NSString *tumblelog;
+@property (readonly) NSURL *rebloggedFromURL;
+@property (readonly) NSString *rebloggedFromName;
+@property (readonly) NSString *rebloggedFromTitle;
+@property (readonly) NSURL *rebloggedRootURL;
+@property (readonly) NSString *rebloggedRootName;
+@property (readonly) NSString *rebloggedRootTitle;
+
 @property (readonly) BOOL bookmarklet;
 @property (readonly) NSUInteger unixtimestamp;
 @property (readonly) NSUInteger width;
 @property (readonly) NSUInteger height;
-@property (nonatomic, retain, readonly) NSDate *dateGMT;
-@property (nonatomic, retain, readonly) NSString *reblogKey;
-@property (nonatomic, retain, readonly) NSString *posturl;
-@property (nonatomic, retain, readonly) NSString *urlWithSlug;
+@property (readonly) NSDate *dateGMT;
+@property (readonly) NSString *reblogKey;
+@property (readonly) NSURL *posturl;
+@property (readonly) NSURL *urlWithSlug;
 
+-(NSData *) sendXHRToURL:(NSString *)url PostValues:(NSDictionary *)postValues;
+-(id)initWithEndPoint:(NSString *)api;
+-(id)initWithBasePost:(JOTumblrPost *)post;
 @end
--- Classes/JOTumblrPost.m
+++ Classes/JOTumblrPost.m
@@ -2,24 +2,165 @@
 //  JOTumblrPost.m
 //  JOTumblr
 //
-//  Created by Jeremy Foo on 5/12/10.
-//  Copyright 2010 ORNYX. All rights reserved.
+//  Created by Jeremy Foo on 3/24/10.
+//  Copyright 2010 THIRDLY. All rights reserved.
 //
+//  The MIT License
+//  
+//  Copyright (c) 2010 Jeremy Foo
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import "JOTumblrPost.h"
 
+#define HTTP_USER_AGENT @"JOTumblr"
+
 @implementation JOTumblrPost
+@synthesize postid, apiendpoint;
 @synthesize type, generator, date, private, tags, format, group, slug;
 @synthesize state, sendToTwitter;
 @synthesize bookmarklet, unixtimestamp, width, height, dateGMT, reblogKey, posturl, urlWithSlug;
+@synthesize liked, noteCount, tumblelog, rebloggedFromURL, rebloggedFromName, rebloggedFromTitle, rebloggedRootURL, rebloggedRootName, rebloggedRootTitle;
 
--(id)init {
+-(id)initWithEndPoint:(NSString *)api {
 	if (self = [super init]) {
+		apiendpoint = [api retain];
+		self.state = @"draft";
 	}
 	return self;
 }
 
--(void)dealloc {
+-(id)initWithBasePost:(JOTumblrPost *)post {
+	if (self = [super init]) {
+		apiendpoint = [post.apiendpoint retain];
+		self.postid = post.postid;
+		posturl = [post.posturl retain];
+		urlWithSlug = [post.urlWithSlug retain];
+		self.type = post.type;
+		self.slug = post.slug;
+		reblogKey = [post.reblogKey retain];
+		self.format = post.format;
+		width = post.width;
+		height = post.height;
+		bookmarklet = post.bookmarklet;
+		self.private = post.private;
+		unixtimestamp = post.unixtimestamp;
+		
+		liked = post.liked;
+		noteCount = post.noteCount;
+		
+		dateGMT = [post.dateGMT retain];
+		self.date = post.date;
+	}
+	return self;
+}
+
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict {	
+	if ([elementName isEqualToString:@"post"]) {
+		self.postid = [attributeDict objectForKey:@"id"];
+		posturl = [[NSURL URLWithString:[attributeDict objectForKey:@"url"]] retain];
+		urlWithSlug = [[NSURL URLWithString:[attributeDict objectForKey:@"url-with-slug"]] retain];
+		self.type = [attributeDict objectForKey:@"type"];
+		self.slug = [attributeDict objectForKey:@"slug"];
+		reblogKey = [[attributeDict objectForKey:@"reblog-key"] retain];
+		self.format = [attributeDict objectForKey:@"format"];
+		width = [[attributeDict objectForKey:@"width"] intValue];
+		height = [[attributeDict objectForKey:@"height"] intValue];
+		bookmarklet = [[attributeDict objectForKey:@"bookmarklet"] boolValue];
+		self.private = [[attributeDict objectForKey:@"private"] boolValue];
+		unixtimestamp = [[attributeDict objectForKey:@"unix-timestamp"] intValue];
+		
+		liked = [[attributeDict objectForKey:@"liked"] boolValue];
+		noteCount = [[attributeDict objectForKey:@"note-count"] intValue];
+		
+		NSDateFormatter *inputFormatter = [[NSDateFormatter alloc] init];
+		
+		//for date-gmt
+		[inputFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss zzz"];
+		dateGMT = [[inputFormatter dateFromString:[attributeDict objectForKey:@"date-gmt"]] retain];
+		
+		//for date
+		[inputFormatter setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss"];
+		self.date = [inputFormatter dateFromString:[attributeDict objectForKey:@"date"]];
+		
+		[inputFormatter release];
+	}
+}
+
+
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
+}
+
+
+-(NSData *) sendXHRToURL:(NSString *)url PostValues:(NSDictionary *)postValues {
+	
+	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+	
+	if (postValues != nil) {
+		
+		NSMutableString *post = [NSMutableString string];
+		for (NSString *key in postValues) {
+			if ([post length] > 0) {
+				[post appendString:@"&"];
+			}
+			[post appendFormat:@"%@=%@",key,[postValues objectForKey:key]];
+		}
+		
+		NSLog(@"Post String: %@", post);
+		NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+		NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+		[request setHTTPMethod:@"POST"];
+		[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+		[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+		[request setValue:HTTP_USER_AGENT forHTTPHeaderField:@"User-Agent"];
+		[request setHTTPBody:postData];
+		
+	}
+	
+	[request setURL:[NSURL URLWithString:url]];
+	
+	NSData *recvData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+	
+	[request release];
+	
+	return recvData;
+}
+
+
+-(void)dealloc {	
+	[currentElement release], currentElement = nil;
+	[currentString release], currentString = nil;
+	
+	[apiendpoint release], apiendpoint = nil;
+	[postid release], postid = nil;
+	[type release], type = nil;
+	[generator release], generator = nil;
+	[date release], date = nil;
+	[tags release], tags = nil;
+	[format release], format = nil;
+	[group release], group = nil;
+	[slug release], slug = nil;
+	[state release], state = nil;
+	[dateGMT release], dateGMT = nil;
+	[reblogKey release], reblogKey = nil;
+	[posturl release], posturl = nil;
+	[urlWithSlug release], urlWithSlug = nil;
 	[super dealloc];
 }
 
