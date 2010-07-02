eef2e070a02c76ec3be1f6d41b26b7c18f2869b6
Jeremy <jeremyfoo@gmail.com>
2010-07-02 13:05:58 +0800

    replaced missing files

--- JOTumblr.m
+++ JOTumblr.m
@@ -1,14 +1,33 @@
 #import <Foundation/Foundation.h>
-#import "JOTumblrPhotoPost.h"
+#import "JOTumblrRegularPost.h"
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-	JOTumblrPhotoPost *photo = [[[JOTumblrPhotoPost alloc] init] autorelease];
+	JOTumblrRegularPost *post = [[[JOTumblrRegularPost alloc] init] autorelease];
+	NSMutableDictionary *postvalues = [NSMutableDictionary dictionary];
+	[postvalues setObject:@"echoz@thirdly.org" forKey:@"email"];
+	[postvalues setObject:@"EchoZr0x" forKey:@"password"];
+	[postvalues setObject:@"1" forKey:@"likes"];
 	
-	photo.source = @"asdsaa";
-	NSLog(@"%@", photo.source);
-
+	NSLog(@"%@", [[[NSString alloc] initWithData:[post sendXHRToURL:@"http://www.tumblr.com/api/dashboard" PostValues:postvalues] encoding:NSASCIIStringEncoding] autorelease]);
+	
+	
+	/*
+	NSData *test = [post sendXHRToURL:@"http://www.tumblr.com/api/dashboard" PostValues:postvalues];
+	
+	NSXMLParser *parser = [[NSXMLParser alloc] initWithData:test];
+	[parser setDelegate:post];
+	[parser setShouldProcessNamespaces:NO];
+	[parser setShouldReportNamespacePrefixes:NO];
+	[parser setShouldResolveExternalEntities:NO];
+	
+	[parser parse];
+	[parser release];
+	
+	NSLog(@"%@: %@",post.title, post.body);
+	 */
+	
     [pool drain];
     return 0;
 }
